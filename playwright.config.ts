027f5ed27373a22ddf935d682b360d93def5eb84
Nesiba Spahić <nesiba.spahic@zira.com.ba>
2025-02-24 13:06:04 +0100

    Added new Playwright setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,8 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL:
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    baseURL: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "retry-with-trace",
@@ -68,9 +67,9 @@ export default defineConfig({
     // },
     {
       name: "Google Chrome",
-      use: { ...devices["Desktop Chrome"], channel: "chrome" },
-    },
-  ],
+      use: { ...devices["Desktop Chrome"], channel: "chrome", headless: false }
+    }
+  ]
 
   /* Run your local dev server before starting the tests */
   // webServer: {
