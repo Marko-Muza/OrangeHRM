871594f2eb57354c2a3d5597dbadcfe77a0d752b
Nesiba Spahić <nesiba.spahic@zira.com.ba>
2025-02-26 08:25:21 +0100

    navigation.spec file is removed

--- tests/smoke/navigation.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from "../../fixtures/fixtures";
-
-test.describe("Navigation Page", () => {
-  test("Navigate", async ({ loginPage, navigation }) => {
-    await navigation.openPIMPage();
-  });
-});
